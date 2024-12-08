0bb2bda3cee69dcb20ff6507172635e83977dce8
Arseniiy Kapshtyk <kapshtykas@gmail.com>
2024-12-08 23:05:35 +0200

    Remove debug logging from refresh token route and middleware; add CORS headers in next.config.js

--- app/api/auth/refresh-token/route.ts
+++ app/api/auth/refresh-token/route.ts
@@ -1,5 +1,4 @@
 // eslint-disable-next-line @typescript-eslint/require-await, @typescript-eslint/no-unused-vars
 export async function POST(request: Request) {
-  console.log(request.headers);
   return Response.json({ message: "Ok" });
 }
